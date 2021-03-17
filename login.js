e1567bb9d235606ec078a8cb7500081b698e7668
Bar Tzadok <bar4488@gmail.com>
2021-03-17 21:06:02 +0200

    login

--- login.js
+++ login.js
@@ -1,2 +1,28 @@
+var loginClick;
 
-window.onload = move_to_login;
+function login() {
+    getLoginParameters(setLoginInput);
+}
+
+function getLoginParameters(callback) {
+    chrome.storage.sync.get("loginParams", function(params){
+        if(params == null) {
+            alert("We dont have your information. Type it now and save it for the next time!");
+            // change button click to saveDetails
+            loginClick = document.getElementsByClassName("subBottun")[0].onclick;
+            document.getElementsByClassName("subBottun")[0].onclick = saveDetails;
+        }
+        else {
+            callback(params.username, params.id, params.password);
+        }
+    });
+}
+
+function setLoginParameters(params, callback) {
+    chrome.storage.sync.set({"loginParams": params}, function(){
+        //saved!
+        callback();
+    });
+}
+
+window.onload = login;
