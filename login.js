1d3ba695ec7e93d3488af271c82bcfd990b5c797
roybogin <roybog00@gmail.com>
2021-03-17 21:09:35 +0200

    let

--- login.js
+++ login.js
@@ -10,10 +10,10 @@ function saveDetails()
 {
     if(confirm("Do you want to save these details?"))
     {
-        username = document.getElementsByName("Ecom_User_ID")[0].value;
-        id = document.getElementsByName("Ecom_User_Pid")[0].value;
-        password = document.getElementsByName("Ecom_Password")[0].value;
-        dict = {
+        let username = document.getElementsByName("Ecom_User_ID")[0].value;
+        let id = document.getElementsByName("Ecom_User_Pid")[0].value;
+        let password = document.getElementsByName("Ecom_Password")[0].value;
+        let dict = {
             "username": username,
             "id": id,
             "password": password
