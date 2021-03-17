fdf68ef04b78e12212fa83f62755a1a064b9b642
roybogin <roybog00@gmail.com>
2021-03-17 21:04:21 +0200

    basic login

--- login.js
+++ login.js
@@ -1,2 +1,25 @@
+function setLoginInputs(username, id, password)
+{
+    document.getElementsByName("Ecom_User_ID")[0].value = username;
+    document.getElementsByName("Ecom_User_Pid")[0].value = id;
+    document.getElementsByName("Ecom_Password")[0].value = password;
+    document.getElementsByName("IDPLogin")[0].sumbit();
+}
 
-window.onload = move_to_login;
+function saveDetails()
+{
+    if(confirm("Do you want to save these details?"))
+    {
+        username = document.getElementsByName("Ecom_User_ID")[0].value;
+        id = document.getElementsByName("Ecom_User_Pid")[0].value;
+        password = document.getElementsByName("Ecom_Password")[0].value;
+        dict = {
+            "username": username,
+            "id": id,
+            "password": password
+        };
+        setLoginParameters(dict, loginClick);
+    }
+}
+
+window.onload = login;
